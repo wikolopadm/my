33f9ce51d1c7b6c3f1d63ee77de43599ebc60571
Laurent PERRET <75728070+wikolopadm@users.noreply.github.com>
2021-03-05 11:17:06 +0100

    Update nikTO.cmd

--- nikTO.cmd
+++ nikTO.cmd
@@ -1,1 +1,1 @@
-@sudo C:\Tools\perl\perl\bin\perl c:\tools\nikto\nikto.pl -h domaine.com:8447 -p 1-1000 -C all
+@sudo C:\Tools\perl\perl\bin\perl c:\tools\nikto\nikto.pl -h domaine.com:80 -p 1-1000 -C all
